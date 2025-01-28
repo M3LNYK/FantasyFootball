a850591d39fabbc91cce3550f9c78a6aad816601
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2025-01-28 11:34:41 +0100

    feat: clean main method

--- scrapper.R
+++ scrapper.R
@@ -7,12 +7,13 @@ library(xml2)  # Not used in this specific code, but might be helpful for future
 
 extract_player_stats_links <- function(competition_url) {
   tryCatch({
-    # player_stats_url <- "https://www.fotmob.com/_next/data/pvJAuiZ8YEQispGXaw-zM/en/leagues/47/stats/premier-league/players.json?season=2023-2024&lng=en&id=47&tab=stats&slug=premier-league&slug=players"
-
-    api <- "https://www.fotmob.com/_next/data/bhtACeKZ4e_Lvxde8cpLT/en/leagues/47/stats/premier-league/players.json?lng=en&id=47&tab=stats&slug=premier-league&slug=players"
-    main_json <- GET(api,
+    api_link <- "https://www.fotmob.com/_next/data/bhtACeKZ4e_Lvxde8cpLT/en/leagues/47/stats/premier-league/players.json?lng=en&id=47&tab=stats&slug=premier-league&slug=players"
+    main_json <- GET(api_link,
                      user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"))
-    main_json$request
+
+    # Analysis of main_json
+
+
 
   }, error = function(e) {
     message("Error: ", e$message)
