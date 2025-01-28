1e52cc7852a694bafd369b3e78e9c6648e9761f8
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2025-01-28 12:43:10 +0100

    feat: add time var

--- scrapper.R
+++ scrapper.R
@@ -45,10 +45,13 @@ extract_player_stats_links <- function(api_link, save_path = NULL) {
 }
 
 # Usage example:
-api_link <- "https://www.fotmob.com/_next/data/bhtACeKZ4e_Lvxde8cpLT/en/leagues/47/stats/premier-league/players.json?lng=en&id=47&tab=stats&slug=premier-league&slug=players"
+api_link <- "https://www.fotmob.com/_next/data/uKh5nioc0sOEP3_mHPgRL/en/leagues/47/stats/premier-league/players.json?lng=en&id=47&tab=stats&slug=premier-league&slug=players"
+
+time <- Sys.Date()
+path <- "Project/Main/temp/"
 
 # Save to current working directory
-json_result <- extract_player_stats_links(api_link, save_path = "player_stats")
+json_result <- extract_player_stats_links(api_link, save_path = path)
 
 # Or save to specific path
 # json_result <- extract_player_stats_links(api_link, save_path = "C:/Users/YourName/Desktop/player_stats")
